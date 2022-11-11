2af0ab1a84d11e4ccaaeb303da32b64d1527d4c2
codenameyujeong <salatosa1@gmail.com>
2022-11-11 17:51:22 +0900

    테스트

--- src/test/ch06/Car.java
+++ src/test/ch06/Car.java
@@ -1,11 +1,30 @@
 package test.ch06;
 
 public class Car {
-	//1.필드: 객체의 데이터를 저장
-	String company = "현대자동차";
-	String model = "그랜저";
-	String color = "검정";
-	int maxSpeed = 350;
-	boolean start;//시동여부
-	int speed;
+
+   Car(String model) {
+      this.model = model;
+   }
+
+   Car(String model, String color) {
+//      this.model = model;
+//      this.color = color;
+	   this(model, color, 250);
+   }
+
+   // 자바에서는 생성자를 명시하지 않으면 컴파일 과정에서 생성자를 생성해준다.
+   Car(String model, String color, int maxSpeed) {
+      // this.model은 아래에 있는 String model이고 key값 model은 위쪽 String model이다!!!
+      this.model = model;
+      this.color = color;
+      this.maxSpeed = maxSpeed;
+   }
+
+   // 1. 필드 : 객체의 데이터를 저장
+   String color = "검정";
+   String model = "그랜저";
+   String company = "현대자동차";
+   boolean start; // 시동여부
+   int maxSpeed = 350;
+   int speed;
 }
